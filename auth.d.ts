531d110e18e40c7089baeb52f8d9dee8f8d8282e
Marina Caro <marina.caro@vasscompany.com>
2025-10-06 17:47:56 +0200

    AuthJS

---
+++ auth.d.ts
@@ -0,0 +1,13 @@
+// Archivo de definición
+
+import type { DefaultSession, DefaultUser } from "@auth/core/types";
+
+declare module "@auth/core/types" {
+  interface User extends DefaultUser {
+    role?: string;
+  }
+
+  interface Session extends DefaultSession {
+    user: User;
+  }
+}
