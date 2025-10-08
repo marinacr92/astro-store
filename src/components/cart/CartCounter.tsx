47e0357fc8c637a0605db479894e93f6976e0c24
Marina Caro <marina.caro@vasscompany.com>
2025-10-08 17:25:14 +0200

    View Tansitions

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -12,7 +12,7 @@ export const CartCounter = () => {
   }, []);
 
   return (
-    <a href="/cart" className="relative inline-block">
+    <a href="/cart" className="relative inline-block mr-6">
       {$itemsInCart > 0 && (
         <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
           {$itemsInCart}
