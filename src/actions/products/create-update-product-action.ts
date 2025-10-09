79036f07f3fe4caf3f19b9cfe5838d2c13ba6525
Marina Caro <marina.caro@vasscompany.com>
2025-10-09 16:36:00 +0200

    Section 16: Cloudinary

--- src/actions/products/create-update-product-action.ts
+++ src/actions/products/create-update-product-action.ts
@@ -1,5 +1,6 @@
+import { ImageUpload } from "@/utils/image-upload";
 import { defineAction } from "astro:actions";
-import { db, eq, Product } from "astro:db";
+import { db, eq, Product, ProductImage } from "astro:db";
 import { z } from "astro:schema";
 import { getSession } from "auth-astro/server";
 import { v4 as UUID } from "uuid";
@@ -58,16 +59,46 @@ export const createUpdateProduct = defineAction({
       ...rest,
     };
 
-    console.log(product);
+    const queries: any = [];
 
     if (!form.id) {
-      await db.insert(Product).values(product);
+      queries.push(db.insert(Product).values(product));
     } else {
-      await db.update(Product).set(product).where(eq(Product.id, id));
+      queries.push(db.update(Product).set(product).where(eq(Product.id, id)));
     }
 
-    console.log(imageFiles);
+    // Imágenes
+    const secureUrls: string[] = [];
 
+    if (
+      form.imageFiles &&
+      form.imageFiles.length > 0 &&
+      form.imageFiles[0].size > 0
+    ) {
+      const urls = await Promise.all(
+        form.imageFiles.map((file) => ImageUpload.upload(file))
+      );
+
+      secureUrls.push(...urls);
+    }
+
+    secureUrls.forEach((imageUrl) => {
+      const imageObj = {
+        id: UUID(),
+        image: imageUrl,
+        productId: product.id,
+      };
+
+      queries.push(db.insert(ProductImage).values(imageObj));
+    });
+
+    // imageFiles?.forEach(async (imageFile) => {
+    //   if (imageFile.size <= 0) return;
+
+    //   const url = await ImageUpload.upload(imageFile);
+    // });
+
+    await db.batch(queries);
     return product;
   },
 });
